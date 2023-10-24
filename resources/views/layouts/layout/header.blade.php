3c19631af824d40ef78e91b89e27dc5a856ac822
Kodjo AMONTO <idrock2016@gmail.com>
2023-10-24 11:52:03 +0200

    deuxième

---
+++ resources/views/layouts/layout/header.blade.php
@@ -0,0 +1,11 @@
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<h1  class="lead">SWEET FABRIQUE</h1>
+				<p class="bg-dark">Bénifissiez d'une Formation professionnelle <a href="#"><b>Sweet Fabrique</b></a></p>
+				<p><a  class="btn btn-success btn-lg" href="{{url('/inscription')}}" role="button">INSCRIPTION</a> <a class="btn btn-action btn-lg" href="{{url('/connexion')}}"  role="button">CONNECTER VOUS</a></p>
+			</div>
+		</div>
+	</header>
+	<!-- /Header -->
